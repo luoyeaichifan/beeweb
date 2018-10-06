f47b4308e853b67519babca79bd414b50efb14f4
kunliang <kunliang@bj-m-213096a.local>
2018-10-06 08:31:21 +0800

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-
+git pull
 ps aufx | grep beeweb | grep -v grep | awk -F " " '{print $2}' | xargs -I {} echo {}
 ps aufx | grep beeweb | grep -v grep | awk -F " " '{print $2}' | xargs -I {} kill -9 {}
 cd $GOPATH/src/github.com/luoyeaichifan/beeweb
