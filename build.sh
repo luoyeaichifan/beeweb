54d93152bb91981165342d953ab0a36f3c606bb3
kunliang <kunliang@bj-m-213096a.local>
2018-10-06 08:16:07 +0800

    build

---
+++ build.sh
@@ -0,0 +1,6 @@
+
+ps aufx | grep beeweb | grep -v grep | awk -F " " '{print $2}' | xargs -I {} echo {}
+ps aufx | grep beeweb | grep -v grep | awk -F " " '{print $2}' | xargs -I {} kill -9 {}
+cd $GOPATH/src/github.com/luoyeaichifan/beeweb
+go build
+./beeweb &
