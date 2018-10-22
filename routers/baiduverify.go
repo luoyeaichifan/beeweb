7986415d5c9b18e20da80aaf57d617b0a92ad8e1
kunliang <kunliang@staff.weibo.com>
2018-10-22 21:37:01 +0800

    test

--- routers/baiduverify.go
+++ routers/baiduverify.go
@@ -1,6 +1,9 @@
 package routers
 
-
+import (
+	"html/template"
+	"log"
+)
 
 // HomeRouter serves home page.
 type BaiduVerify struct {
@@ -11,4 +14,11 @@ type BaiduVerify struct {
 func (this *BaiduVerify) Get() {
 	//this.Data["IsHome"] = true
 	this.TplName = "baidu_verify_Hbvw7qAUmJ.html"
+
+	t, err := template.ParseFiles("baidu_verify_Hbvw7qAUmJ.html")
+	if err != nil {
+		log.Println("err:",err)
+		return
+	}
+	t.Execute(this.baseRouter.Controller.Ctx.ResponseWriter, nil)
 }
